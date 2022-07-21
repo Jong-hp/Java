d835d8f15b1b4af20b84ea92e2584b36244918a0
parkjonghyeok <qkr7405@gmail.com>
2022-07-21 18:04:08 +0900

    220721

---
+++ java_work/Hello/src/test/main/MainClass.java
@@ -0,0 +1,22 @@
+package test.main;
+
+public class MainClass {
+	public static void main(String[] args) {
+		System.out.println("main 메소드가 시작 되었습니다.");
+		//let num1=10;
+		int num1=10;
+		//let num2=10.1;
+		double num2=10.1;
+		//let isRun=true;
+		boolean isRun=true;
+		//let myName="박종혁";
+		String myName="박종혁";
+		
+		num1=20;
+		num2=20.2;
+		isRun=false;
+		myName="해골";
+		
+		System.out.println("main 메소드가 종료 됩니다.");
+	}
+}
